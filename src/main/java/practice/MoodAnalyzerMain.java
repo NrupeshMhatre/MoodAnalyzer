a3fd7341a7375fc9362a4970d0f563412bcfc62f
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-13 15:27:48 +0530

    In Uc1 TestCase1 and TestCase2 add

---
+++ src/main/java/practice/MoodAnalyzerMain.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class MoodAnalyzerMain {
+public String analyMood(String msg) {
+	if(msg.contains(("sad"))) {
+		return "SAD";
+	}
+	else {
+		return "HAPPY";
+		
+	}
+}
+}
---
+++ src/test/java/test/MoodAnalyzerTest.java
@@ -0,0 +1,22 @@
+package test;
+import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import practice.MoodAnalyzerMain;
+
+public class MoodAnalyzerTest {
+	    @Test
+	    public void sadMood() {
+	        MoodAnalyzerMain mo = new MoodAnalyzerMain();
+	        String mood = mo.analyMood("Output is a sad ");
+	        assertEquals(mood,"SAD");
+	    }
+	    @Test
+	    public void happyMood() {
+	        MoodAnalyzerMain mo = new MoodAnalyzerMain();
+	        String mood = mo.analyMood("OutPut is a happy ");
+	        assertEquals(mood,"HAPPY");
+	}
+}
+
